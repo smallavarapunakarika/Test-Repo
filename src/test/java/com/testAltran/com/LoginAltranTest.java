462da84fb3c524a13c561bbbd88d5b27610b8674
smallavarapunakarika <smallavarapunakarika@L-DE-47C3J72.europe.corp.altran.com>
2018-11-15 16:03:56 +0100

    First update with creation of repositorzy

---
+++ src/test/java/com/testAltran/com/LoginAltranTest.java
@@ -0,0 +1,11 @@
+package com.testAltran.com;
+
+import org.testng.annotations.Test;
+
+public class LoginAltranTest {
+  @Test
+  public void login() {
+	  System.out.println("This is the first test");
+	  
+  }
+}
